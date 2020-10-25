faa0ccd091ce74b5aaf20f23cadccc8cf78c0ab6
bartosz Minczuk <bminczu@gmail.com>
2020-10-25 16:28:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,5 +128,9 @@ def game_hash
 end
 
 def players
-  game_hash[:home][:players]. merge {game_hash[:home][:players]}
+  game_hash[:home][:players]. merge {game_hash[:away][:players]}
+end
+
+def num_points_scored(player_name)
+  players[player_name][:points]
 end
